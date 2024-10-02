54715002010e7f88a131ce582e16d12230019d79
David Valente <d.valente@davidvalente.xyz>
2024-10-02 17:10:42 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# hugo-pathway-pro
+# Pathway Pro
+
 A professional pathway landing page theme for Hugo.
+
+## Features
+
+## Installation
+
+## Configuration
---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+(() => {
+  // <stdin>
+  tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          "primary": "#2D3748",
+          /* Configure your colours */
+          "secondary": "#F59E0B",
+          "accent": "#10B981",
+          "background": "#F3F4F6",
+          "text": "#1F2937"
+        },
+        fontFamily: {
+          "serif": ["Merriweather", "serif"],
+          /* Configure your fonts */
+          "lato": ["Source Sans Pro", "sans-serif"]
+        }
+      }
+    }
+  };
+})();
